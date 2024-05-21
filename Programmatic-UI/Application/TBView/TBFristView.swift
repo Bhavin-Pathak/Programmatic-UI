c83f04f333b42c4f779b386f0586fe6d432062f3
Bhavin8289 <bupadhyay277@gmail.com>
2024-05-21 12:10:38 +0530

    adding new screen to root

---
+++ Programmatic-UI/Application/TBView/TBFristView.swift
@@ -0,0 +1,17 @@
+//
+//  TBFristView.swift
+//  Programmatic-UI
+//
+//  Created by MacBookPro on 31/02/1946 Saka.
+//
+
+import UIKit
+
+class TBFristView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+}
