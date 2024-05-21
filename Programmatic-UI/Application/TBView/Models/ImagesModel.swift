1edb598aa7cdbe16236042ff414b0204990780a3
Bhavin8289 <bupadhyay277@gmail.com>
2024-05-21 12:12:48 +0530

    model added

---
+++ Programmatic-UI/Application/TBView/Models/ImagesModel.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesModel.swift
+//  Programmatic-UI
+//
+//  Created by MacBookPro on 31/02/1946 Saka.
+//
+
+import UIKit
+
+struct ImagesModel {
+    
+    var images : UIImage
+    var titel: String
+    
+}
