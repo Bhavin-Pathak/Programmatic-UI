473c76adff9ee7fec104c400d5d9659111837bd8
Bhavin8289 <bupadhyay277@gmail.com>
2024-05-21 11:41:00 +0530

    chore: adding images assets or defector codes

---
+++ Programmatic-UI/Application/Constants/Images.swift
@@ -0,0 +1,21 @@
+//
+//  Images.swift
+//  Programmatic-UI
+//
+//  Created by MacBookPro on 31/02/1946 Saka.
+//
+import UIKit
+
+struct Images {
+    
+    //MARK: Static Images That Added As Assets on Project
+    static let babygold = UIImage(named: "baby-gold")
+    static let dogesh = UIImage(named: "dogesh")
+    static let goldii = UIImage(named: "goldii")
+    static let husky = UIImage(named: "husky")
+    static let pinki = UIImage(named: "pinki")
+    static let pumpy = UIImage(named: "pumpy")
+    static let rosedog = UIImage(named: "rose-dog")
+    static let white = UIImage(named: "white")
+    
+}
---
+++ Programmatic-UI/Application/ViewController/SecondView.swift
@@ -0,0 +1,16 @@
+//
+//  SecondView.swift
+//  Programmatic-UI
+//
+//  Created by MacBookPro on 31/02/1946 Saka.
+//
+
+import UIKit
+
+class SecondView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
